6a989504da012f27e21d835b0b29fe3a73254e45
Roberto <roberto.felipe467@gmail.com>
2025-07-24 12:20:27 -0300

    Método para login feito

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import StackNavigator from './src/navigation/StackNavigator';
-import { useState } from 'react';
+import Login from './src/Pages/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { useFonts as useOswald, Oswald_400Regular, Oswald_700Bold } from '@expo-google-fonts/oswald';
 
@@ -10,6 +11,7 @@ import { Text, View } from 'react-native';
 export default function App() {
 
   const [ loading, setLoading ] = useState(true);
+  const [ logado, setLogado ] = useState(false);
   const [oswaldLoaded] = useOswald({ Oswald_400Regular, Oswald_700Bold});
  
     if(!oswaldLoaded){
@@ -20,11 +22,13 @@ export default function App() {
       )
     } 
 
+    
+
   return (
     <NavigationContainer>
       <SafeAreaProvider>
         <StatusBar style="auto" />
-        <StackNavigator/>       
+        {logado ? <StackNavigator /> : <Login setLogado={setLogado} />}
       </SafeAreaProvider>
     </NavigationContainer>
   );
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,20 +1,27 @@
-import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { Colors } from '../../Utils/Colors';
 import { useState } from 'react';
 import { login } from '../../Services/AuthService';
 
-export default function Login() {
+export default function Login( {setLogado}: { setLogado: (value: boolean) => void }) {
 
   const [ loading, setLoading ] = useState(false);
   const [ email, setEmail ] = useState('');
   const [ senha, setSenha ] = useState('');
 
   const handleLogin = async () => {
+    setLoading(true);
+
+    if(!email || !senha){
+      console.error('Email e senha são obrigatórios');
+      setLoading(false);
+      return;
+    }
 
     try {
-      setLoading(true);
       const response = await login(email, senha);
       console.log('Login realizado com sucesso:', response);
+      setLogado(true);
     } catch (error: any) {
       console.error('Erro ao realizar login:', error.message);
       
@@ -28,9 +35,11 @@ export default function Login() {
     <Text style={{ color: Colors.Branco, fontSize: 28, marginBottom: 20, fontWeight: 'bold' }}>Faça o Login</Text>
      <TextInput placeholder="Email" style={styles.input} value={email} onChangeText={setEmail} />
      <TextInput placeholder="Senha" style={styles.input} secureTextEntry value={senha} onChangeText={setSenha} />
-     <TouchableOpacity style={styles.btnSubmitLogin} onPress={handleLogin}>
-       <Text style={{ color: Colors.Branco, textAlign: 'center', fontSize: 18 }}>{loading? 'Carregando...' : 'Entrar'}</Text>
-     </TouchableOpacity>
+     {loading ? <ActivityIndicator color={Colors.Amarelo} size={36} /> : 
+        <TouchableOpacity style={styles.btnSubmitLogin} onPress={handleLogin}>
+          <Text style={{ color: Colors.Branco, textAlign: 'center', fontSize: 18 }}>Entrar</Text>
+        </TouchableOpacity>}
+     
      <Text style={{ color: Colors.Branco, marginTop: 20 }}>Novo por aqui? 
         <TouchableOpacity>
             <Text style={{ fontWeight: 'bold', color: Colors.Amarelo }}> Crie uma conta grátis</Text>
